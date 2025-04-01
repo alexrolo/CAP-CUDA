9392f809af5e90b59c2ff97f9c1ad97c36588cc1
Alex <UO281827@uniovi.es>
2025-04-01 20:00:50 +0200

    Auto update

---
+++ equations/cuda_functions.cuh
@@ -0,0 +1,18 @@
+#ifndef CUDA_FUNCTIONS_H
+#define CUDA_FUNCTIONS_H
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+#include <cuda.h>
+
+void CudaEventCreate(cudaEvent_t *event);
+void CudaEventDestroy(cudaEvent_t event);
+void CudaMalloc(void **devPtr, size_t size);
+void CudaFree(void *devPtr);
+void CudaMemcpy(void *dst, const void *src, size_t count, cudaMemcpyKind kind);
+void CudaEventRecord(cudaEvent_t event, cudaStream_t stream);
+void CudaEventSynchronize(cudaEvent_t event);
+void CudaEventElapsedTime(float *ms, cudaEvent_t start, cudaEvent_t end);
+
+#endif
---
+++ equations/log.h
@@ -0,0 +1,13 @@
+#ifndef LOG_H
+#define LOG_H
+
+#include <stdio.h>
+
+/**
+ * Logs a message to stdout.
+ * 
+ * @param message The message to log
+ */
+void log_message(const char *message);
+
+#endif // LOG_H
