4ec9b986aff3b1c03b29d3db45773c4bc6049595
Alex <UO281827@uniovi.es>
2025-04-08 19:23:06 +0200

    Auto update

--- equations/equations_cuda.cu
+++ equations/equations_cuda.cu
@@ -1,7 +1,7 @@
-#include "cuda_functions.cuh"
+#include "functions_cuda.cuh"
 #include "functions.h"
 
-__global__ void gauss_jordan(unsigned int size, double *mat)
+__global__ void gauss_jordan(unsigned int size, double *matrix)
 {
     unsigned int const current_column = blockIdx.x * blockDim.x + threadIdx.x;
 
@@ -13,7 +13,7 @@ __global__ void gauss_jordan(unsigned int size, double *mat)
     double max_value = 0;
     for (current_row = current_column; current_row < size; current_row++)
     {
-        double current_value = *(mat + current_row + current_column * size);
+        double current_value = *(matrix + current_row + current_column * size);
         if (current_value < 0)
             current_value = -current_value; // Take absolute value for comparison
         if (current_value > max_value)
@@ -27,41 +27,42 @@ __global__ void gauss_jordan(unsigned int size, double *mat)
     if (max_row != current_column)
         for (current_row = 0; current_row < size; current_row++)
         {
-            double temp = *(mat + current_column + current_row * size);
-            *(mat + current_column + current_row * size) = *(mat + max_row + current_row * size);
-            *(mat + max_row + current_row * size) = temp;
+            double temp = *(matrix + current_column + current_row * size);
+            *(matrix + current_column + current_row * size) = *(matrix + max_row + current_row * size);
+            *(matrix + max_row + current_row * size) = temp;
         }
 
     // Make the diagonal element equal to 1
-    double divisor = *(mat + current_column + current_column * size);
+    double divisor = *(matrix + current_column + current_column * size);
     for (unsigned int i = 0; i <= size; i++)
-        *(mat + current_column * size + i) /= divisor; // Normalize the pivot row
+        *(matrix + current_column * size + i) /= divisor; // Normalize the pivot row
 
     // Make zeros in the current column
     for (unsigned int i = 0; i < size; i++)
     {
         if (i != current_column)
         {
-            double multiplier = *(mat + i * size + current_column) / *(mat + current_column * size + current_column);
+            double multiplier = *(matrix + i * size + current_column) / *(matrix + current_column * size + current_column);
             for (unsigned int j = 0; j <= size; j++)
-                *(mat + i * size + j) -= multiplier * *(mat + current_column * size + j); // Eliminate the current column
+                *(matrix + i * size + j) -= multiplier * *(matrix + current_column * size + j); // Eliminate the current column
         }
     }
 }
 
-double* solve_equation_with_gpu(unsigned int size, double* mat, double* sol)
+double* solve_equation_with_gpu(unsigned int size, double* matrix, double* sol)
 {
     unsigned int i;
-    double* d_mat;
+    double* d_matrix;
     cudaEvent_t start, end;
     float ms;
 
     // Allocate memory on the GPU
     size_t total_size = size * size * sizeof(double*);
-    CudaMalloc((void*) &d_mat, total_size);
-    CudaMemcpy(d_mat, mat, total_size, cudaMemcpyHostToDevice);
+    CudaMalloc((void**) &d_matrix, total_size);
+    CudaMemcpy(d_matrix, matrix, total_size, cudaMemcpyHostToDevice);
     
     // TODO: Define dim3 grid dimensions
+    const unsigned int threads = 32;
     dim3 threadsPerBlock(threads, threads);
     dim3 numBlocks((size + threadsPerBlock.x - 1) / threadsPerBlock.x,
                    (size + threadsPerBlock.y - 1) / threadsPerBlock.y);
@@ -71,18 +72,18 @@ double* solve_equation_with_gpu(unsigned int size, double* mat, double* sol)
 
     cudaEventRecord(start, 0);
     // TODO: Call the kernel function
-    gauss_jordan<<<numBlocks, threadsPerBlock>>>(size, d_mat);
+    gauss_jordan<<<numBlocks, threadsPerBlock>>>(size, d_matrix);
 
     CudaEventSynchronize(end);
     CudaEventRecord(end, 0);
     CudaEventElapsedTime(&ms, start, end);
 
-    CudaFree(d_mat);
+    CudaFree(d_matrix);
     CudaEventDestroy(start);
     CudaEventDestroy(end);
 
     for (i = 0; i < size ; i++)
-        sol[i] = mat[i][size];
+        sol[i] = *(matrix + i * size + size);
 
     return sol;
 }
@@ -90,7 +91,7 @@ double* solve_equation_with_gpu(unsigned int size, double* mat, double* sol)
 int main(int argc, char *argv[])
 {
     unsigned int size; // Square matrix
-    double **mat, *sol;
+    double *matrix, *sol;
     clock_t start, end;
     double seconds;
     char* buffer = (char*)malloc(128);
@@ -109,22 +110,22 @@ int main(int argc, char *argv[])
     }
 
     // Allocate memory
-    mat = allocate_matrix(size);
+    matrix = allocate_matrix(size);
     sol = (double *)malloc(size * sizeof(double));
 
     // Generate random matrix
     srand(time(NULL));
-    generate_matrix(size, mat);
+    generate_matrix(size, matrix);
 
     unsigned int i;
 
     start = clock();
-    solve_equation_with_gpu(size, mat, sol);
+    solve_equation_with_gpu(size, matrix, sol);
     end = clock();
 
     // The solution is in the last column
     for (i = 0; i < size; i++)
-        sol[i] = mat[i][size];
+	*(sol + i) = *(matrix + i * size + size);
 
     seconds = (double)(end - start) / CLOCKS_PER_SEC;
     printf("Execution time (seconds): %.5f\n", seconds);
@@ -133,18 +134,17 @@ int main(int argc, char *argv[])
     printf("System solution:\n");
     for (unsigned int i = 0; i < size; i++)
     {
-        printf("x%d = %.3f\n", i, mat[i][size]);
-        sol[i] = mat[i][size];
+        printf("x%d = %.3f\n", i, *(sol + i));
     }
 
     // Check if the solution is correct
-    if (check_equation_system(size, mat, sol))
+    if (check_equation_system(size, matrix, sol))
         printf("The solution is correct.\n");
     else
         printf("The solution is incorrect.\n");
 
     // Free matrix
-    free_matrix(size, mat);
+    free(matrix);
     free(sol);
     free(buffer);
 
