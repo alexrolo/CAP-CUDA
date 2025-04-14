b3634137a881a5e5ed6bf0a6457338a02bc46c21
Alex <alejandro.rod.lop@gmail.com>
2025-04-14 17:21:07 +0200

    Updated makefile and added comparer

---
+++ equations/comparer.cpp
@@ -0,0 +1,58 @@
+#include <iostream>
+#include <vector>
+
+#include "functions.h"
+#include "gauss_jordan.h"
+
+int main(int argc, char **argv)
+{
+    std::vector<unsigned int> sizes = {
+        2, 4, 8, 16, 32, 64, 128, 256, 512, 1024};
+    const unsigned int iterations = 32;
+
+    double *matrix, *original_matrix = NULL, *sol;
+    clock_t start, end;
+    double seconds;
+
+    bool was_valid = false;
+
+    // Generate random matrix
+    srand(time(NULL));
+
+    std::cout << "SIZE;ARCH;TIME" << std::endl;
+    for (auto size : sizes)
+    {
+        seconds = 0;
+        was_valid = false;
+        for (unsigned int iteration = 0; iteration < iterations; iteration++)
+        {
+            while (!was_valid)
+            {
+                // Allocate memory
+                matrix = allocate_matrix(size);
+                sol = (double *)malloc(size * sizeof(double));
+                generate_matrix(size, matrix);
+                original_matrix = copy_matrix(size, matrix, original_matrix);
+
+                start = clock();
+                gauss_jordan(size, matrix);
+                end = clock();
+
+                // The solution is in the last column
+                for (unsigned int i = 0; i < size; i++)
+                    sol[i] = *(matrix + i * (size + 1) + size);
+
+                seconds += (double)(end - start) / CLOCKS_PER_SEC;
+
+                // Check if the solution is correct
+                was_valid = check_equation_system(size, matrix, sol);
+
+                // Free matrix
+                free(matrix);
+                free(original_matrix);
+                free(sol);
+            }
+        }
+        std::cout << size << ";" << "CPU" << ";" << seconds / iterations << std::endl;
+    }
+}
