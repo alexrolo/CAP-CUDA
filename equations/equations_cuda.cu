8ef78a740733a1648a0e1a49cde2af17398e5b42
Alex <alejandro.rod.lop@gmail.com>
2025-04-14 18:56:40 +0200

    fix cuda equations

--- equations/equations_cuda.cu
+++ equations/equations_cuda.cu
@@ -1,67 +1,14 @@
 #include "functions_cuda.cuh"
-
-__global__ void gauss_jordan(unsigned int size, double *matrix)
-{
-    unsigned int const current_column = blockIdx.x * blockDim.x + threadIdx.x;
-    const unsigned int actual_columns = size + 1;
-
-    if (current_column >= size)
-        return; // Out of bounds
-
-    // Find maximum in current column for partial pivoting
-    unsigned int max_row = 0, current_row = 0;
-    double max_value = 0;
-    for (current_row = current_column; current_row < size; current_row++)
-    {
-        double current_value = *(matrix + current_row * actual_columns + current_column);
-        if (current_value < 0)
-            current_value = -current_value; // Take absolute value for comparison
-        if (current_value > max_value)
-        {
-            max_row = current_row;
-            max_value = current_value;
-        }
-    }
-
-    // Swap rows
-    if (max_row != current_column)
-    {
-        for (unsigned int i = 0; i < actual_columns; i++)
-        {
-            unsigned int current_idx = current_column * actual_columns + i;
-            unsigned int max_idx = max_row * actual_columns + i;
-            double temp = *(matrix + current_idx);
-            *(matrix + current_idx) = *(matrix + max_idx);
-            *(matrix + max_idx) = temp;
-        }
-    }
-
-    // Make the diagonal element equal to 1
-    double divisor = *(matrix + current_column * actual_columns + current_column);
-    for (unsigned int i = 0; i < actual_columns; i++)
-        *(matrix + current_column * actual_columns + i) /= divisor; // Normalize the pivot row
-
-    // Make zeros in the current column
-    for (unsigned int i = 0; i < size; i++)
-    {
-        if (i != current_column)
-        {
-            double multiplier = *(matrix + i * actual_columns + current_column) / *(matrix + current_column * actual_columns + current_column);
-            for (unsigned int j = 0; j <= size; j++)
-                *(matrix + i * actual_columns + j) -= multiplier * *(matrix + current_column * actual_columns + j); // Eliminate the current column
-        }
-    }
-}
+#include "gauss_jordan_cuda.cuh"
 
 double *solve_equation_with_gpu(unsigned int size, unsigned int threads, double *matrix, double *sol)
 {
-
     double *d_matrix;
     cudaEvent_t start, end;
     float ms;
 
     // Allocate memory on the GPU
-    size_t total_size = size * size * sizeof(double *);
+    size_t total_size = size * (size + 1) * sizeof(double);
     CudaMalloc((void **)&d_matrix, total_size);
     CudaMemcpy(d_matrix, matrix, total_size, cudaMemcpyHostToDevice);
 
@@ -86,7 +33,7 @@ double *solve_equation_with_gpu(unsigned int size, unsigned int threads, double 
     CudaEventDestroy(end);
 
     for (unsigned int i = 0; i < size; i++)
-        sol[i] = *(matrix + i * size + size);
+        sol[i] = *(matrix + i * (size + 1) + size);
 
     return sol;
 }
@@ -94,8 +41,7 @@ double *solve_equation_with_gpu(unsigned int size, unsigned int threads, double 
 int main(int argc, char *argv[])
 {
     unsigned int size, threads; // Square matrix
-    double *matrix, *sol;
-    double *original_matrix = NULL;
+    double *matrix, *original_matrix = NULL, *sol;
     clock_t start, end;
     double seconds;
 
---
+++ equations/gauss_jordan_cuda.cu
@@ -0,0 +1,54 @@
+#include "gauss_jordan_cuda.cuh"
+
+__global__ void gauss_jordan(unsigned int size, double *matrix)
+{
+    unsigned int const current_column = blockIdx.x * blockDim.x + threadIdx.x;
+    const unsigned int actual_columns = size + 1;
+
+    if (current_column >= size)
+        return; // Out of bounds
+
+    // Find maximum in current column for partial pivoting
+    unsigned int max_row = 0, current_row = 0;
+    double max_value = 0;
+    for (current_row = current_column; current_row < size; current_row++)
+    {
+        double current_value = *(matrix + current_row * actual_columns + current_column);
+        if (current_value < 0)
+            current_value = -current_value; // Take absolute value for comparison
+        if (current_value > max_value)
+        {
+            max_row = current_row;
+            max_value = current_value;
+        }
+    }
+
+    // Swap rows
+    if (max_row != current_column)
+    {
+        for (unsigned int i = 0; i < actual_columns; i++)
+        {
+            unsigned int current_idx = current_column * actual_columns + i;
+            unsigned int max_idx = max_row * actual_columns + i;
+            double temp = *(matrix + current_idx);
+            *(matrix + current_idx) = *(matrix + max_idx);
+            *(matrix + max_idx) = temp;
+        }
+    }
+
+    // Make the diagonal element equal to 1
+    double divisor = *(matrix + current_column * actual_columns + current_column);
+    for (unsigned int i = 0; i < actual_columns; i++)
+        *(matrix + current_column * actual_columns + i) /= divisor; // Normalize the pivot row
+
+    // Make zeros in the current column
+    for (unsigned int i = 0; i < size; i++)
+    {
+        if (i != current_column)
+        {
+            double multiplier = *(matrix + i * actual_columns + current_column) / *(matrix + current_column * actual_columns + current_column);
+            for (unsigned int j = 0; j <= size; j++)
+                *(matrix + i * actual_columns + j) -= multiplier * *(matrix + current_column * actual_columns + j); // Eliminate the current column
+        }
+    }
+}
