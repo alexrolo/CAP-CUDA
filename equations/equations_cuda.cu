e83dc1394ff948fc90fea3adff4e80ba45aed4ba
rubennmg <rubenmg2015@gmail.com>
2025-04-14 18:05:32 +0200

    New argument. Only print small SEL

--- equations/equations_cuda.cu
+++ equations/equations_cuda.cu
@@ -53,7 +53,7 @@ __global__ void gauss_jordan(unsigned int size, double *matrix)
     }
 }
 
-double *solve_equation_with_gpu(unsigned int size, double *matrix, double *sol)
+double *solve_equation_with_gpu(unsigned int size, unsigned int threads, double *matrix, double *sol)
 {
 
     double *d_matrix;
@@ -65,8 +65,7 @@ double *solve_equation_with_gpu(unsigned int size, double *matrix, double *sol)
     CudaMalloc((void **)&d_matrix, total_size);
     CudaMemcpy(d_matrix, matrix, total_size, cudaMemcpyHostToDevice);
 
-    // TODO: Define dim3 grid dimensions
-    const unsigned int threads = 32;
+    // Define dim3 grid dimensions
     dim3 threadsPerBlock(threads, threads);
     dim3 numBlocks((size + threadsPerBlock.x - 1) / threadsPerBlock.x,
                    (size + threadsPerBlock.y - 1) / threadsPerBlock.y);
@@ -94,22 +93,28 @@ double *solve_equation_with_gpu(unsigned int size, double *matrix, double *sol)
 
 int main(int argc, char *argv[])
 {
-    unsigned int size; // Square matrix
+    unsigned int size, threads; // Square matrix
     double *matrix, *sol;
     double *original_matrix = NULL;
     clock_t start, end;
     double seconds;
 
     // Check if the required arguments are provided
-    if (argc != 2)
+    if (argc != 3)
     {
-        fprintf(stderr, "Usage: %s <size>\n", argv[0]);
+        fprintf(stderr, "Usage: %s <size> <threads_per_block>\n", argv[0]);
         return 1;
     }
 
     if ((size = atoi(argv[1])) < 1)
     {
         fprintf(stderr, "Invalid size: %s\n", argv[1]);
+        return 1;
+    }
+
+    if ((threads = atoi(argv[2])) < 1)
+    {
+        fprintf(stderr, "Invalid threads per block: %s\n", argv[2]);
         return 1;
     }
 
@@ -125,7 +130,7 @@ int main(int argc, char *argv[])
     unsigned int i;
 
     start = clock();
-    solve_equation_with_gpu(size, matrix, sol);
+    solve_equation_with_gpu(size, threads, matrix, sol);
     end = clock();
 
     // The solution is in the last column
@@ -136,12 +141,21 @@ int main(int argc, char *argv[])
     printf("Execution time (seconds): %.5f\n", seconds);
 
     // The solution is in the last column
-    printf("System solution:\n");
-    for (unsigned int i = 0; i < size; i++)
-        printf("x%d = %.3f\n", i, *(sol + i));
+    if (size < 16)
+    {
+        printf("System solution:\n");
+        for (unsigned int i = 0; i < size; i++)
+            printf("x%d = %.3f\n", i, *(sol + i));
+    } else 
+    {
+        printf("System solution is too large to print.\n");
+    }
 
     printf("Checking against original matrix:\n");
-    print_equation_system(size, original_matrix);
+    if (size < 16)
+        print_equation_system(size, matrix);
+    else
+        printf("Matrix is too large to print.\n");
 
     // Check if the solution is correct
     if (check_equation_system(size, original_matrix, sol))
