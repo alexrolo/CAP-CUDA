1c349045917225ffd5f20276416b1ff417d79903
Alex <alejandro.rod.lop@gmail.com>
2025-04-14 17:15:49 +0200

    Abstracted gauss_jordan c from main file

--- equations/equations.c
+++ equations/equations.c
@@ -1,58 +1,7 @@
 #include <time.h>
 
+#include "gauss_jordan.h"
 #include "functions.h"
-
-void gauss_jordan(unsigned int size, double *matrix)
-{
-    unsigned int current_column = 0;
-    const unsigned int actual_columns = size + 1;
-    for (current_column = 0; current_column < size; current_column++)
-    {
-        // Find maximum in current column for partial pivoting
-        unsigned int max_row = 0, current_row = 0;
-        double max_value = 0;
-        for (current_row = current_column; current_row < size; current_row++)
-        {
-            double current_value = *(matrix + current_row * actual_columns + current_column);
-            if (current_value < 0)
-                current_value = -current_value; // Take absolute value for comparison
-            if (current_value > max_value)
-            {
-                max_row = current_row;
-                max_value = current_value;
-            }
-        }
-
-        // Swap rows
-        if (max_row != current_column)
-        {
-            for (unsigned int i = 0; i < actual_columns; i++)
-            {
-                unsigned int current_idx = current_column * actual_columns + i;
-                unsigned int max_idx = max_row * actual_columns + i;
-                double temp = *(matrix + current_idx);
-                *(matrix + current_idx) = *(matrix + max_idx);
-                *(matrix + max_idx) = temp;
-            }
-        }
-
-        // Make the diagonal element equal to 1
-        double divisor = *(matrix + current_column * actual_columns + current_column);
-        for (unsigned int i = 0; i < actual_columns; i++)
-            *(matrix + current_column * actual_columns + i) /= divisor; // Normalize the pivot row
-
-        // Make zeros in the current column
-        for (unsigned int i = 0; i < size; i++)
-        {
-            if (i != current_column)
-            {
-                double multiplier = *(matrix + i * actual_columns + current_column) / *(matrix + current_column * actual_columns + current_column);
-                for (unsigned int j = 0; j <= size; j++)
-                    *(matrix + i * actual_columns + j) -= multiplier * *(matrix + current_column * actual_columns + j); // Eliminate the current column
-            }
-        }
-    }
-}
 
 int main(int argc, char *argv[])
 {
---
+++ equations/gauss_jordan.h
@@ -0,0 +1,6 @@
+#ifndef GAUSS_JORDAN_H
+#define GAUSS_JORDAN_H
+
+void gauss_jordan(unsigned int size, double *matrix);
+
+#endif // GAUSS_JORDAN_H
