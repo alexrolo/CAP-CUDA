9e7088e593a1793f5f894b6eca07a58bcbf10130
rubennmg <rubenmg2015@gmail.com>
2025-04-14 19:06:33 +0200

    ;

--- equations/gauss_jordan_cuda.cuh
+++ equations/gauss_jordan_cuda.cuh
@@ -3,6 +3,6 @@
 
 #include <cuda.h>
 
-__global__ void gauss_jordan(unsigned int size, double *matrix)
+__global__ void gauss_jordan(unsigned int size, double *matrix);
 
 #endif // GAUSS_JORDAN_CUDA_H
