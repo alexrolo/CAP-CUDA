e1a2a09cf9af0d192f7cc9989b3add8beda96dae
Alex <alejandro.rod.lop@gmail.com>
2025-04-14 16:23:45 +0200

    Fixed same pointers on cuda code (allegedly)

--- equations/equations_cuda.cu
+++ equations/equations_cuda.cu
@@ -3,49 +3,54 @@
 __global__ void gauss_jordan(unsigned int size, double *matrix)
 {
     unsigned int const current_column = blockIdx.x * blockDim.x + threadIdx.x;
+    const unsigned int actual_columns = size + 1;
 
     if (current_column >= size)
         return; // Out of bounds
 
     // Find maximum in current column for partial pivoting
-    unsigned int max_row = 0, current_row = 0;
-    double max_value = 0;
-    for (current_row = current_column; current_row < size; current_row++)
-    {
-        double current_value = *(matrix + current_row + current_column * size);
-        if (current_value < 0)
-            current_value = -current_value; // Take absolute value for comparison
-        if (current_value > max_value)
+        unsigned int max_row = 0, current_row = 0;
+        double max_value = 0;
+        for (current_row = current_column; current_row < size; current_row++)
         {
-            max_row = current_row;
-            max_value = current_value;
-        }
-    }
-
-    // Swap rows
-    if (max_row != current_column)
-        for (current_row = 0; current_row < size; current_row++)
-        {
-            double temp = *(matrix + current_column + current_row * size);
-            *(matrix + current_column + current_row * size) = *(matrix + max_row + current_row * size);
-            *(matrix + max_row + current_row * size) = temp;
+            double current_value = *(matrix + current_row * actual_columns + current_column);
+            if (current_value < 0)
+                current_value = -current_value; // Take absolute value for comparison
+            if (current_value > max_value)
+            {
+                max_row = current_row;
+                max_value = current_value;
+            }
         }
 
-    // Make the diagonal element equal to 1
-    double divisor = *(matrix + current_column + current_column * size);
-    for (unsigned int i = 0; i <= size; i++)
-        *(matrix + current_column * size + i) /= divisor; // Normalize the pivot row
-
-    // Make zeros in the current column
-    for (unsigned int i = 0; i < size; i++)
-    {
-        if (i != current_column)
+        // Swap rows
+        if (max_row != current_column)
         {
-            double multiplier = *(matrix + i * size + current_column) / *(matrix + current_column * size + current_column);
-            for (unsigned int j = 0; j <= size; j++)
-                *(matrix + i * size + j) -= multiplier * *(matrix + current_column * size + j); // Eliminate the current column
+            for (unsigned int i = 0; i < actual_columns; i++)
+            {
+                unsigned int current_idx = current_column * actual_columns + i;
+                unsigned int max_idx = max_row * actual_columns + i;
+                double temp = *(matrix + current_idx);
+                *(matrix + current_idx) = *(matrix + max_idx);
+                *(matrix + max_idx) = temp;
+            }
         }
-    }
+
+        // Make the diagonal element equal to 1
+        double divisor = *(matrix + current_column * actual_columns + current_column);
+        for (unsigned int i = 0; i < actual_columns; i++)
+            *(matrix + current_column * actual_columns + i) /= divisor; // Normalize the pivot row
+
+        // Make zeros in the current column
+        for (unsigned int i = 0; i < size; i++)
+        {
+            if (i != current_column)
+            {
+                double multiplier = *(matrix + i * actual_columns + current_column) / *(matrix + current_column * actual_columns + current_column);
+                for (unsigned int j = 0; j <= size; j++)
+                    *(matrix + i * actual_columns + j) -= multiplier * *(matrix + current_column * actual_columns + j); // Eliminate the current column
+            }
+        }
 }
 
 double* solve_equation_with_gpu(unsigned int size, double* matrix, double* sol)
@@ -90,10 +95,9 @@ double* solve_equation_with_gpu(unsigned int size, double* matrix, double* sol)
 int main(int argc, char *argv[])
 {
     unsigned int size; // Square matrix
-    double *matrix, *sol;
+    double *matrix, *original_matrix, *sol;
     clock_t start, end;
     double seconds;
-    char* buffer = (char*)malloc(128);
 
     // Check if the required arguments are provided
     if (argc != 2)
@@ -115,6 +119,7 @@ int main(int argc, char *argv[])
     // Generate random matrix
     srand(time(NULL));
     generate_matrix(size, matrix);
+    original_matrix = copy_matrix(size, matrix, original_matrix);
 
     unsigned int i;
 
@@ -124,7 +129,7 @@ int main(int argc, char *argv[])
 
     // The solution is in the last column
     for (i = 0; i < size; i++)
-	*(sol + i) = *(matrix + i * size + size);
+	    *(sol + i) = *(matrix + i * (size + 1) + size);
 
     seconds = (double)(end - start) / CLOCKS_PER_SEC;
     printf("Execution time (seconds): %.5f\n", seconds);
@@ -132,20 +137,21 @@ int main(int argc, char *argv[])
     // The solution is in the last column
     printf("System solution:\n");
     for (unsigned int i = 0; i < size; i++)
-    {
         printf("x%d = %.3f\n", i, *(sol + i));
-    }
 
+    printf("Checking against original matrix:\n");
+    print_equation_system(size, original_matrix);
+    
     // Check if the solution is correct
-    if (check_equation_system(size, matrix, sol))
+    if (check_equation_system(size, original_matrix, sol))
         printf("The solution is correct.\n");
     else
         printf("The solution is incorrect.\n");
 
     // Free matrix
     free(matrix);
+    free(original_matrix);
     free(sol);
-    free(buffer);
 
     return 0;
 }
