65354e199a3fef43f4d54317adfb51382f7347d0
Alex <UO281827@uniovi.es>
2025-04-18 17:57:34 +0200

    idk what im doing anymore

--- equations/comparer_cuda.cu
+++ equations/comparer_cuda.cu
@@ -21,16 +21,24 @@ double *solve_equation_with_gpu(unsigned int size, unsigned int threads, double 
     dim3 numBlocks((size + threadsPerBlock.x - 1) / threadsPerBlock.x,
                    (size + threadsPerBlock.y - 1) / threadsPerBlock.y);
 
+    print_equation_system(size, matrix);
+
     CudaEventCreate(&start);
     CudaEventCreate(&end);
 
     CudaEventRecord(start);
     // TODO: Call the kernel function
-    gauss_jordan<<<numBlocks, threadsPerBlock>>>(size, d_matrix);
+    // gauss_jordan<<<1, 1>>>(size, d_matrix);
+    gauss_jordan_no_swap<<<numBlocks, threadsPerBlock>>>(size, d_matrix);
+    checkCudaError();
+
+    CudaMemcpy(matrix, d_matrix, total_size, cudaMemcpyDeviceToHost);
 
     CudaEventRecord(end);
     CudaEventSynchronize(end);
     CudaEventElapsedTime(&ms, start, end);
+
+    print_equation_system(size, matrix);
 
     CudaFree(d_matrix);
     CudaEventDestroy(start);
@@ -45,10 +53,12 @@ double *solve_equation_with_gpu(unsigned int size, unsigned int threads, double 
 int main(int argc, char **argv)
 {
     std::vector<unsigned int> sizes = {
-        2, 4, 8, 16, 32, 64, 128, 256, 512, 1024, 2048, 4096};
-    const unsigned int iterations = 32;
+	3};
+    std::vector<unsigned int> test_threads = {
+	8};
+    const unsigned int iterations = 1;
 
-    double *matrix, *original_matrix = NULL, *sol, *d_matrix;
+    double *matrix, *original_matrix = NULL, *sol;
     clock_t start, end;
     double seconds;
     unsigned int total_iterations = 0;
@@ -61,44 +71,47 @@ int main(int argc, char **argv)
     std::cout << "SIZE;ARCH;THRS;TIME;SUCC" << std::endl;
     for (auto size : sizes)
     {
-        const unsigned int half_size = size / 2;
-        for (unsigned int threads = 1; threads <= half_size; threads++)
+        for (auto threads : test_threads)
         {
-            seconds = 0;
-            total_iterations = 0;
-            for (unsigned int iteration = 0; iteration < iterations; iteration++)
-            {
-                was_valid = false;
-                while (!was_valid)
-                {
-                    total_iterations++;
-                    // Allocate memory
-                    matrix = allocate_matrix(size);
-                    sol = (double *)malloc(size * sizeof(double));
-                    generate_matrix(size, matrix);
-                    original_matrix = copy_matrix(size, matrix, original_matrix);
+	    seconds = 0;
+	    total_iterations = 0;
+	    for (unsigned int iteration = 0; iteration < iterations; iteration++)
+	    {
+	    was_valid = false;
+    	    while (!was_valid)
+		{
+		    total_iterations++;
+		    // Allocate memory
+		    matrix = allocate_matrix(size);
+		    sol = (double *)malloc(size * sizeof(double));
+		    generate_matrix(size, matrix);
+		    original_matrix = copy_matrix(size, matrix, original_matrix);
 
-                    start = clock();
-                    solve_equation_with_gpu(size, threads, matrix, sol);
-                    end = clock();
+		    // print_equation_system(size, matrix);
 
-                    // The solution is in the last column
-                    for (unsigned int i = 0; i < size; i++)
-                        sol[i] = *(matrix + i * (size + 1) + size);
+		    start = clock();
+		    solve_equation_with_gpu(size, threads, matrix, sol);
+		    end = clock();
 
-                    seconds += (double)(end - start) / CLOCKS_PER_SEC;
+		    // print_equation_system(size, matrix);
 
-                    // Check if the solution is correct
-                    was_valid = check_equation_system(size, matrix, sol);
+		    // The solution is in the last column
+		    for (unsigned int i = 0; i < size; i++)
+			sol[i] = *(matrix + i * (size + 1) + size);
 
-                    // Free matrix
-                    free(matrix);
-                    free(original_matrix);
-                    free(sol);
-                }
-            }
-            const double success_rate = (double)iterations / total_iterations;
-            std::cout << size << ";" << "GPU" << ";" << threads << ";" << seconds / iterations << ";" << success_rate << std::endl;
+		    seconds += (double)(end - start) / CLOCKS_PER_SEC;
+
+		    // Check if the solution is correct
+		    was_valid = check_equation_system(size, matrix, sol);
+
+		    // Free matrix
+		    free(matrix);
+		    free(original_matrix);
+		    free(sol);
+		}
+	    }
+	    const double success_rate = (double)iterations / total_iterations;
+	    std::cout << size << ";" << "GPU" << ";" << threads << ";" << seconds / iterations << ";" << success_rate << std::endl;
         }
     }
 }
